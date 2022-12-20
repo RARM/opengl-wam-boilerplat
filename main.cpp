088b91222171ec56b1e72854b27281ca8c47e38b
Rodolfo Andrés Rivas Matta <rodolforivasmatta@gmail.com>
2022-12-20 21:44:03 +0000

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <GLES2/gl2.h>
+#include <emscripten.h>
+#include <emscripten/html5.h>
+#include <iostream>
+
+int main(int argc, char** argv) 
+{
+    EmscriptenWebGLContextAttributes attrs;
+    attrs.alpha = false;
+    attrs.depth = true;
+    attrs.stencil = true;
+    attrs.antialias = true;
+    attrs.premultipliedAlpha = false;
+    attrs.preserveDrawingBuffer = false;
+    attrs.failIfMajorPerformanceCaveat = false;
+    attrs.majorVersion = 1;
+    attrs.minorVersion = 0;
+    attrs.enableExtensionsByDefault = false;
+
+    int ctx = emscripten_webgl_create_context("#canvas", &attrs);
+    if(!ctx)
+    {
+        std::cout << "Error: WebGL context could not be created." << std::endl;
+        return -1;
+    }    
+
+    emscripten_webgl_make_context_current(ctx);
+    glClearColor(0,0,0.5,1); // Set the clear color (backgroun) to dark blue.
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    return 0;
+}
